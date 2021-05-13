4c13a2ac9a6d39066a204c42e21d640ad9e9ea67
Skyler Rivera <skylerrivera17@yahoo.com>
2021-05-12 20:19:50 -0400

    trying to figure out heroku package.json setup

--- server/index.js
+++ server/index.js
@@ -5,14 +5,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/public')))
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello from server!' })
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 })
 
 app.listen(PORT, () => {
